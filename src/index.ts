222316e479bffee5015bf37f103a4ab0b39d136c
ch1nto <mastertokung@gmail.com>
2026-04-15 15:08:16 +0700

    2026/04/15 before back to bkk.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello dsdsds").listen(3000);
+const app = new Elysia()
+  .get("/api/hero", () => {
+    return Bun.file("data/hero/hero.json").json();
+  })
+  .get("/api/projects", () => {
+    return Bun.file("data/projects/projects.json").json();
+  })
+  .get("/api/about", () => {
+    return Bun.file("data/about/about.json").json();
+  })
+  .get("/api/contact", () => {
+    return Bun.file("data/contact/contact.json").json();
+  })
+  .get("/public", () => {
+    return Bun.file("public/index.html").text();
+  })
+  .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
